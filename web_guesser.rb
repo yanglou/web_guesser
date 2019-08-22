47f2070fbd062cd243419df6cd7d6777396fcbe6
yanglou <yanglou@ualberta.ca>
2019-08-22 16:20:09 -0700

    I4: limited guesses to 5

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,50 +1,71 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-
-
 SECRET_NUMBER =rand(100)
+
+
 message=""
 color="white"
+
+class Count
+  
+  @@count_guesses=6
+  
+end
+
+
 
 def check_guess(guess)
   input=guess.to_i
   if input > SECRET_NUMBER
     if input-SECRET_NUMBER>5
-      message = "Way too high!"
-      color = "red"
+      message = "Way too high!"     
     else
-      message = "Too high!"
-      color = "#ffe6e6"
+      message = "Too high!"      
     end
    elsif input < SECRET_NUMBER
     if SECRET_NUMBER-input>5
-      message = "Way too low!"
-      color = "red"
+      message = "Way too low!"      
     else
-      message = "Too low!"
-      color = "#ffe6e6"
+      message = "Too low!"      
     end
    elsif input = SECRET_NUMBER
     message = "You got it right! \n The SECRET NUMBER is " + SECRET_NUMBER.to_s
+    reinit
   end
+
+  @@count_guesses -= 1
+
+  if @@count_guesses<=0    
+    message = "You lost. A new number has been generated."
+    reinit
+  end
+
   message
 end
 
-def color_check(message)
+def color_check(message, input)
   if message.include? "Way"
     color = "red"
   elsif message.include? "right!"
     color = "green"
-  else 
+  elsif (input-SECRET_NUMBER).abs<5
     color = "lightpink"
+  else
+    color = "white"
   end
 end
 
+def reinit
+  @SECRET_NUMBER = rand(100)
+  @@count_guesses=5
+end
+
 get '/' do  
+  
   guess=params["guess"]
   message = check_guess(guess)  
-  color = color_check(message) 
-  erb :index, :locals => {:color => color, :number => SECRET_NUMBER, :message => message}
+  color = color_check(message, guess.to_i) 
+  erb :index, :locals => {:count_guesses => @@count_guesses, :color => color, :number => SECRET_NUMBER, :message => message}
 
 end
