6104938b8d279d2125aee78cc1b0f9a97eb604f9
yanglou <yanglou@ualberta.ca>
2019-08-22 21:08:40 -0700

    I4: bugs

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,49 +6,58 @@ SECRET_NUMBER =rand(100)
 
 message=""
 color="white"
-cheat=false
+cheat=nil
 cheat_message = ""
-
-class Count  
-  @@count_guesses=6  
-end
-
+@@count_guesses = 5
+guess=nil
 
 get '/' do  
   
-  cheat_message = "The SECRET NUMBER is " + SECRET_NUMBER.to_s if params["cheat"] = true
+  
   guess=params["guess"]
-  check_cheat(cheat)
-  message = check_guess(guess)  
-  color = color_check(message, guess.to_i) 
-  erb :index, :locals => {:cheat_message => cheat_message, :count_guesses => @@count_guesses, :color => color, :number => SECRET_NUMBER, :message => message}
+  cheat= params["cheat"]
+  
+  if guess !=nil
+    message = check_guess(guess)    
+    color = color_check(message, guess.to_i)
+  end
+
+  if cheat == "true"
+    cheat_message = "The SECRET NUMBER is " + SECRET_NUMBER.to_s
+  end
+  
+   
+  erb :index, :locals => {:cheat => cheat,:cheat_message => cheat_message, :count_guesses => @@count_guesses, :color => color, :number => SECRET_NUMBER, :message => message}
 
 end
 
 def check_guess(guess)
   input=guess.to_i
   if input > SECRET_NUMBER
-    if input-SECRET_NUMBER>5
+    if (input-SECRET_NUMBER).abs>5
       message = "Way too high!"     
     else
       message = "Too high!"      
     end
-   elsif input < SECRET_NUMBER
-    if SECRET_NUMBER-input>5
+  elsif input < SECRET_NUMBER
+    if (SECRET_NUMBER-input).abs>5
       message = "Way too low!"      
     else
       message = "Too low!"      
     end
-   elsif input = SECRET_NUMBER
-    message = "You got it right! \n The SECRET NUMBER is " + SECRET_NUMBER.to_s
+  elsif input = SECRET_NUMBER
+    message = "You got it right! \n The SECRET NUMBER is " + SECRET_NUMBER.to_s+ ". A new number has been generated."
     reinit
+    @@count_guesses +=1
+  else
+    message=""
   end
 
-  @@count_guesses -= 1
-
+  @@count_guesses -= 1 
   if @@count_guesses<=0    
     message = "You lost. A new number has been generated."
     reinit
+    
   end
 
   message
@@ -59,6 +68,8 @@ def color_check(message, input)
     color = "red"
   elsif message.include? "right!"
     color = "green"
+  elsif message.include? "lost"
+    color = "white"
   elsif (input-SECRET_NUMBER).abs<5
     color = "lightpink"
   else
@@ -69,4 +80,6 @@ end
 def reinit
   @SECRET_NUMBER = rand(100)
   @@count_guesses=5
+  @guess=nil
+  @cheat=nil
 end
